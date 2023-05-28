7fb33fd72023bb9d426519bf19effc4229cdf4f9
Malarit <78352453+Malarit@users.noreply.github.com>
2023-05-28 19:03:54 +0300

    utils

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -7,7 +7,7 @@ import { CookieOptions } from "express";
 
 // Headers
 const headers = {
-  "Access-Control-Allow-Origin": "http://localhost:5174",
+  "Access-Control-Allow-Origin": "http://localhost:5173",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
   "Access-Control-Allow-Headers": "X-Requested-With,content-type",
   "Access-Control-Allow-Credentials": "true",
